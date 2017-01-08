5396f56a9f088d89a09d2a425071694bef304d31
Colin Jaffe <abbreviatedman@Colins-MacBook-Pro.local>
2017-01-07 19:24:23 -0500

    first real commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ClickContainer from './ClickContainer';
+
+ReactDOM.render(
+  <ClickContainer />,
+  document.getElementById('app');
+);
